0142d1297674f1c1fd10732dce8ba4b4d110a220
Zain Adam <zainadam.id@gmail.com>
2022-06-27 02:19:01 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# rsb-algorithm
-Residential Social Benefit (RSB) algorithm is an algorithm based on Eigenvector Centrality and Fuzzy Comprehensive Evaluation to conclude and compare two qualitative assessment.
+# RSB Algorithm
+
+Residential Social Benefit (RSB) Algorithm is an algorithm based on Eigenvector Centrality and Fuzzy Comprehensive Evaluation to conclude and compare two qualitative assessment.
+
+## Installation
+
+Install using composer:
+
+```bash
+composer require sensasi-delight/rsb-algorithm
+```
+
+## Usage
+
+The usage examples of this library are also available on [examples folder](https://github.com/sensasi-delight/eigenvector-centrality-php/tree/main/examples).
+
+1. Define the assesment scale.
+
+    ```php
+    $assessment_scale = [
+        "Very Low" => 1,
+        "Low" => 2,
+        "Medium" => 3,
+        "High" => 4,
+        "Very High" => 5,
+    ];
+    ```
+
+2. Define two datasets.
+
+    ```php
+    $datasets = [
+        ['dataset_from'] => [
+            ['question1' => [
+                ['respondent1' => 5],
+                ['respondent2' => 3],
+                ['respondent3' => 4],
+                ['respondent4' => 2],
+                ['respondent5' => 3]
+            ], 'question2' => [
+                ['respondent1' => 4],
+                ['respondent2' => 3],
+                ['respondent3' => 3],
+                ['respondent4' => 2],
+                ['respondent5' => 1]
+            ] ... ]
+        ], ['dataset_against'] => [
+            ['question1' => [
+                ['respondent1' => 3],
+                ['respondent2' => 5],
+                ['respondent3' => 2],
+                ['respondent4' => 3],
+                ['respondent5' => 4]
+            ], 'question2' => [
+                ['respondent1' => 2],
+                ['respondent2' => 3],
+                ['respondent3' => 4],
+                ['respondent4' => 5],
+                ['respondent5' => 1]
+            ] ... ]
+        ]
+    ];
+    ```
+
+3. Make a RSBAlgorithm object and set the properties
+
+    ```php
+    $RSBAlgorithm = new RSBAlgorithm(
+        $assessment_scale,
+        $datasets
+    );
+    ```
+
+    or
+
+    ```php
+    $RSBAlgorithm = new RSBAlgorithm;
+
+    $RSBAlgorithm->assessment_scale = $assessment_scale;
+    $RSBAlgorithm->datasets = $datasets;
+    ```
+
+4. Get the score
+    overall score:
+
+    ```php
+    print_r(RSBAlgorithm->score)
+    ```
+
+    questions/criterias/variables score
+
+    ```php
+    print_r(RSBAlgorithm->scores);
+    ```
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement". Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project.
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`).
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`).
+4. Push to the Branch (`git push origin feature/AmazingFeature`).
+5. Open a Pull Request.
+
+## License
+
+The code is released under the MIT license.
+
+## Contact
+
+Email - [zainadam.id@gmail.com](mailto:zainadam.id@gmail.com?subject=[GitHub]%20EigenvectorCentralityPHP)
+
+Twitter - [@sensasi_DELIGHT](https://twitter.com/sensasi_DELIGHT)
---
+++ src/RSBAlgorithm.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace SensasiDelight;
+
+use MathPHP\LinearAlgebra\MatrixFactory;
+use SensasiDelight\EigenvectorCentralityPHP\Graph;
+
+
+class RSBAlgorithm
+{
+	public $scores;
+	public $score;
+	public $weights;
+
+	private $datasets;
+	private $assessment_scale;
+
+	public function __construct(array $assessment_scale, array $datasets) {
+		$this->__set('assessment_scale', $assessment_scale);
+		$this->__set('datasets', $datasets);
+	}
+
+	public function __set($name, $value)
+	{
+		if ($name == "assessment_scale") {
+			$this->assessment_scale = $value;
+		}
+
+		if ($name == "datasets") {
+			if (count($value) !== 2) {
+				throw new \Exception("datasets format unrecognizeable");
+			}
+	
+			$this->datasets = $value;
+		}
+
+
+
+
+		if ($this->isRequiredFilled()) {
+			$this->calculate();
+		}
+	}
+
+	private function set_weights()
+	{
+		$g = new Graph;
+
+		$questions = current($this->datasets);
+		foreach ($questions as $qId => $respondents) {
+			$question_ids[] = $qId;
+			foreach ($respondents as $rId => $answer) {
+				$g->add_edge($qId, $rId);
+				$respondent_ids[] = $rId;
+			}
+		}
+
+		$respondent_ids = array_unique($respondent_ids);
+
+		$eigenvectors = $g->get_eigenvector_centrality();
+		$questions_weights = self::toItsPercentage(array_intersect_key($eigenvectors, array_flip($question_ids)));
+		$respondent_weights = self::toItsPercentage(array_intersect_key($eigenvectors, array_flip($respondent_ids)));
+
+		$this->weights = array_merge($questions_weights, $respondent_weights);
+	}
+
+	private function calculate()
+	{
+		$this->set_weights();
+
+		$w_vector = MatrixFactory::create([array_values($this->assessment_scale)]);
+		$dataset_ids = array_keys($this->datasets);
+		$dataset_id_from = current($dataset_ids);
+		$dataset_id_against = end($dataset_ids);
+
+		$scores = [];
+
+		foreach ($this->datasets as $dataset_id => $dataset) {
+			foreach ($dataset as $qId => $respondents) {
+				$evaluation_vector = array_fill(0, count($this->assessment_scale), 0);
+
+				foreach ($respondents as $rId => $answer) {
+					$evaluation_vector[$answer - 1] += $this->weights[$rId];
+				}
+
+				$evaluation_matrices[$dataset_id][$qId] = self::toItsPercentage($evaluation_vector);
+
+				$r = MatrixFactory::create([array_values($evaluation_matrices[$dataset_id][$qId])]);
+
+				if (empty($scores[$qId])) {
+					$scores[$qId] = new \stdClass;
+				}
+
+				$scores[$qId]->{$dataset_id} = $r->multiply($w_vector->transpose())->get(0, 0);
+			}
+		}
+
+
+		foreach ($scores as $qId => &$score) {
+			$score->gap = $score->{$dataset_id_from} - $score->{$dataset_id_against};
+		}
+
+		$this->scores = $scores;
+
+
+
+		// calculate orevall score
+		$score = new \stdClass;
+
+		$weights_matrix = array_values(array_intersect_key($this->weights, current($evaluation_matrices)));
+		$weights_matrix = MatrixFactory::create([$weights_matrix]);
+
+		foreach ($evaluation_matrices as $dataset_id => $evaluation_matrix) {
+			$evaluation_matrix = array_values($evaluation_matrix);
+			$evaluation_matrix = MatrixFactory::create($evaluation_matrix);
+			$score_vector = $weights_matrix->multiply($evaluation_matrix);
+
+			$score->{$dataset_id} = $score_vector->multiply($w_vector->transpose())->get(0, 0);
+		}
+
+		$score->gap = $score->{$dataset_id_from} - $score->{$dataset_id_against};
+		$this->score = $score;
+	}
+
+
+	/**
+	 * Helper
+	 */
+	public static function toItsPercentage(array $numbers)
+	{
+		$sum = array_sum($numbers);
+		foreach ($numbers as $i => $number) {
+			$numbers[$i] = $number / $sum;
+		}
+
+		return $numbers;
+	}
+
+	private function isRequiredFilled()
+	{
+		return $this->datasets AND $this->assessment_scale;
+	}
+}
